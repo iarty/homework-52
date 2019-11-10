9c1f2c04a13986b5541cd1c64d4d75a008d7cc0c
Barbonov Artem <abarbonov@gmail.com>
2019-11-11 01:05:28 +0600

    add bootstrap

--- src/components/lottery/lottery.js
+++ src/components/lottery/lottery.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
 
 export default class index extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
 
-      </div>
+      </Fragment>
     )
   }
 }
