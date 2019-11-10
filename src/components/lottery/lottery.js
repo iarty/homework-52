7553c1faee74b165ca9f3b0ac3b94aaa6c1a2d9b
Barbonov Artem <abarbonov@gmail.com>
2019-11-11 01:26:48 +0600

    create HTML markup

--- src/components/lottery/lottery.js
+++ src/components/lottery/lottery.js
@@ -1,9 +1,24 @@
-import React, { Component,Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Button } from 'reactstrap';
+import Container from 'reactstrap/lib/Container';
+
 
 export default class index extends Component {
   render() {
     return (
       <Fragment>
+        <Container>
+          <div className="text-center">
+            <h1 className="my-5">Lottery</h1>
+            <Button color="danger" className="mb-5">New numbers!</Button>
+            <div className="d-flex mt-5 justify-content-around">
+              <span className="display-3" style={{border:'1px solid red',borderRadius:'50%',width:'70px'}}>1</span>
+              <span className="display-3" style={{border:'1px solid red',borderRadius:'50%',width:'70px'}}>2</span>
+              <span className="display-3" style={{border:'1px solid red',borderRadius:'50%',width:'70px'}}>3</span>
+              <span className="display-3" style={{border:'1px solid red',borderRadius:'50%',width:'70px'}}>4</span>
+            </div>
+          </div>
+        </Container>
 
       </Fragment>
     )
