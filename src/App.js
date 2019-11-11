d610362111a0547ef2d4ff1d4b953c1cad12dfba
Barbonov Artem <abarbonov@gmail.com>
2019-11-11 12:09:01 +0600

    finally project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Lottery from './components/lottery/lottery.js'
 
 export default class index extends Component {
-  state = {
-    num: []
-  }
-  rand = () => {
-    let arr = [];
-    for (let index = 0; index < 4; index++) {
-      arr.push(Math.floor(Math.random() * (36 - 5 + 1)) + 5);
+    state = {
+        num: []
     }
-    return arr.sort((a, b) => { return a - b })
-  }
-  genNum = () => {
-    this.setState({
-      num: this.rand()
-    })
-  }
-  render = () => {
-    return (<Lottery genNum={this.genNum} num={this.state.num} />)
-  }
+
+    rand = () => {
+        let arr = [];
+        do {
+            let num = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            arr.push(num);
+            arr = arr.filter((item, index) => {
+                return arr.indexOf(item) === index
+            });
+        } while (arr.length < 4);
+        return arr.sort((a, b) => {
+            return a - b
+        });
+    }
+
+    genNum = () => {
+        this.setState({
+            num: this.rand()
+        })
+    }
+
+    render = () => {
+        return (<Lottery genNum={this.genNum} num={this.state.num}/>)
+    }
 }
