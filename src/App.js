af9478f050eb3044f7628e879a298127f532af16
Barbonov Artem <abarbonov@gmail.com>
2019-11-11 13:09:54 +0600

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Lottery from './components/lottery/lottery.js'
 
 export default class index extends Component {
-    state = {
-        num: []
-    }
+  state = {
+    num: []
+  }
 
-    rand = () => {
-        let arr = [];
-        do {
-            let num = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
-            arr.push(num);
-            arr = arr.filter((item, index) => {
-                return arr.indexOf(item) === index
-            });
-        } while (arr.length < 5);
-        return arr.sort((a, b) => {
-            return a - b
-        });
-    }
+  numGen = () => {
+    let arr = [];
+    do {
+      let num = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+      arr.push(num);
+      arr = arr.filter((item, index) => {
+        return arr.indexOf(item) === index
+      });
+    } while (arr.length < 5);
+    return arr.sort((a, b) => {
+      return a - b
+    });
+  }
 
-    genNum = () => {
-        this.setState({
-            num: this.rand()
-        })
-    }
+  genNum = () => {
+    let numArr = [...this.state.num];
+    numArr = [...this.numGen()];
+    this.setState({ num:numArr })
+  }
 
-    render = () => {
-        return (<Lottery genNum={this.genNum} num={this.state.num}/>)
-    }
+  render = () => {
+    return (<Lottery genNum={this.genNum} num={this.state.num} />)
+  }
 }
