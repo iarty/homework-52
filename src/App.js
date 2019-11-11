061605689323e6856d0211919b6dc8dfe0947094
Barbonov Artem <abarbonov@gmail.com>
2019-11-11 11:21:30 +0600

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Lottery from './components/lottery/lottery.js'
 
-function App() {
-  return (
-    <Lottery />
-  );
+export default class index extends Component {
+  state = {
+    num: []
+  }
+  rand = () => {
+    let arr = [];
+    for (let index = 0; index < 4; index++) {
+      arr.push(Math.floor(Math.random() * (36 - 5 + 1)) + 5);
+    }
+    return arr.sort((a, b) => { return a - b })
+  }
+  genNum = () => {
+    this.setState({
+      num: this.rand()
+    })
+  }
+  render = () => {
+    return (<Lottery genNum={this.genNum} num={this.state.num} />)
+  }
 }
-
-export default App;
--- src/components/lottery/lottery.js
+++ src/components/lottery/lottery.js
@@ -1,33 +1,25 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Button } from 'reactstrap';
 import Container from 'reactstrap/lib/Container';
 
 
-export default class index extends Component {
-  num = [];
-  genRand = () => {
-    for (let index = 0; index < 4; index++) {
-      this.num.push(Math.floor(Math.random() * (36 - 5 + 1)) + 5);
-    }
-    return this.num.sort((a, b) => { return a - b })
-  }
-  render = () => {
-    const sortNum = this.genRand();
-    return (
-      <Fragment>
-        <Container>
-          <div className="text-center">
-            <h1 className="my-5">Lottery</h1>
-            <Button color="danger" className="mb-5">New numbers!</Button>
-            <div className="d-flex mt-5 justify-content-around">
-              <span className="display-3" style={{ border: '1px solid red', borderRadius: '50%', width: '70px' }}>{sortNum[0]}</span>
-              <span className="display-3" style={{ border: '1px solid red', borderRadius: '50%', width: '70px' }}>{sortNum[1]}</span>
-              <span className="display-3" style={{ border: '1px solid red', borderRadius: '50%', width: '70px' }}>{sortNum[2]}</span>
-              <span className="display-3" style={{ border: '1px solid red', borderRadius: '50%', width: '70px' }}>{sortNum[3]}</span>
-            </div>
+
+const Lottery = (props) => {
+  return (
+    <Fragment>
+      <Container>
+        <div className="text-center">
+          <h1 className="my-5">Lottery</h1>
+          <Button color="danger" className="mb-5" onClick={props.genNum}>New numbers!</Button>
+          <div className="d-flex mt-5 justify-content-around">
+            <span className="display-3" style={{ borderBottom: '2px solid red', width: '70px', height: '70px' }}>{props.num[0]}</span>
+            <span className="display-3" style={{ borderBottom: '2px solid red', width: '70px', height: '70px' }}>{props.num[1]}</span>
+            <span className="display-3" style={{ borderBottom: '2px solid red', width: '70px', height: '70px' }}>{props.num[2]}</span>
+            <span className="display-3" style={{ borderBottom: '2px solid red', width: '70px', height: '70px' }}>{props.num[3]}</span>
           </div>
-        </Container>
-      </Fragment>
-    )
-  }
+        </div>
+      </Container>
+    </Fragment>)
 }
+
+export default Lottery;
